e7c1b1c4b95dab33a232f238d28cdce4c7821c37
Duarte Roso <duarteroso@users.noreply.github.com>
2024-03-14 20:02:34 +0100

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import Footer from '$lib/footer.svelte';
 	import Header from '$lib/header.svelte';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics'
+
+	inject({ mode: dev ? 'development' : 'production' });
+	
 </script>
 
 <Header />
