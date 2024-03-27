259710d2f74ce527da5f973d2807885828c03b8e
Duarte Roso <duarteroso@users.noreply.github.com>
2024-03-27 09:58:49 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import Header from '$lib/header.svelte';
 
 	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics'
+	import { inject } from '@vercel/analytics';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	inject({ mode: dev ? 'development' : 'production' });
